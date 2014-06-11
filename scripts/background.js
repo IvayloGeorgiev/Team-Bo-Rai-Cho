f919b3e5bcb9001d54d2701bb4b44c155bbc6399
Stefanov <stefanov2081@gmail.com>
2014-06-11 22:00:21 +0300

    Quality Code
    Cleared some code, for quality code. No JS Lint errors.

--- scripts/background.js
+++ scripts/background.js
@@ -1,6 +1,7 @@
 ﻿/*global document: false*/
 /*global window: false*/
 /*global Raphael: false*/
+/*global Image: false*/
 
 (function () {
     "use strict";
@@ -11,6 +12,19 @@
         rect,
         x,
         y,
+        frame1,
+        frame2,
+        frame1Url,
+        frame2Url,
+        frameHeight,
+        frame1Image,
+        frame2Image,
+        frame3Image,
+        time,
+        frame1Anim,
+        frame2Anim,
+        frame3Anim;
+
 
     width = window.screen.width;
     height = window.screen.height;
@@ -18,32 +32,32 @@
     rect = paper.rect(0, 0, width, height);
     rect.attr('fill', '#000');
 
-    var x = 0;
-    var y = 0;
+    x = 0;
+    y = 0;
 
-    var frame1 = new Image();
-    var frame2 = new Image();
+    frame1 = new Image();
+    frame2 = new Image();
 
-    var frame1Url = '/images/outerSpace.jpg';
-    var frame2Url = '/images/outerSpaceFlipped.jpg';
+    frame1Url = '/images/outerSpace.jpg';
+    frame2Url = '/images/outerSpaceFlipped.jpg';
 
     frame1.src = frame1Url;
     frame2.src = frame2Url;
 
-    // Apparently Chrome does not work with fram1.height... This is a fix for Chrome
-    var frameHeight = 1920;
+    // Apparently Chrome does not work with frame1.height... This is a fix for Chrome
+    frameHeight = 1920;
 
-    var frame1Image = paper.image(frame1Url, x, y - frameHeight * 2, width, frameHeight);
-    var frame2Image = paper.image(frame2Url, x, y - frameHeight, width, frameHeight);
-    var frame3Image = paper.image(frame1Url, x, y, width, frameHeight);
+    frame1Image = paper.image(frame1Url, x, y - frameHeight * 2, width, frameHeight);
+    frame2Image = paper.image(frame2Url, x, y - frameHeight, width, frameHeight);
+    frame3Image = paper.image(frame1Url, x, y, width, frameHeight);
 
-    var time = 300000;
-    var frame1Anim = Raphael.animation({ x: 0, y: frameHeight - frameHeight }, time);
-    var frame2Anim = Raphael.animation({ x: 0, y: frameHeight }, time);
-    var frame3Anim = Raphael.animation({ x: 0, y: frameHeight * 2 }, time);
+    time = 300000;
 
+    frame1Anim = Raphael.animation({ x: 0, y: frameHeight - frameHeight }, time);
+    frame2Anim = Raphael.animation({ x: 0, y: frameHeight }, time);
+    frame3Anim = Raphael.animation({ x: 0, y: frameHeight * 2 }, time);
 
     frame1Image.animate(frame1Anim.repeat(Infinity), '<>');
     frame2Image.animate(frame2Anim.repeat(Infinity), '<>');
     frame3Image.animate(frame3Anim.repeat(Infinity), '<>');
-})();
+}());
